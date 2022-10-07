bfb11a68352533eba7576c65daf0bfcca6c77212
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2022-10-07 15:00:25 +0300

    nav model düzeltidli

--- Tekce.Store/WebUI/Models/NavModel.cs
+++ Tekce.Store/WebUI/Models/NavModel.cs
@@ -1,52 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc.ActionConstraints;
 using System.Text;
 using WebUI._keenthemes.libs;
-
 namespace System.Web.MVC
 {
     public class NavModel
     {
         private IKTTheme kTTheme;
         private List<Menu> Root = new List<Menu>();
-
         public NavModel(IKTTheme kTTheme)
         {
             this.kTTheme = kTTheme;
             Root.Add(DashBoard());
-
-
         }
-
         public Menu DashBoard()
         {
-            var menu = new Menu("Anasayfa", "/", kTTheme.getSvgIcon("duotune/general/gen025.svg",
-                                             "svg-icon svg-icon-2"));
+            var menu = new Menu("Anasayfa", "/", kTTheme.getSvgIcon("duotune/general/gen025.svg","svg-icon svg-icon-2"));
             var mainMenu= new Menu("Anasayfa","/");
-            var reports = new Menu("Raporlar","/Dashboards/Privacy");
+            var reports = new Menu("Gizlilik","/Dashboards/Privacy");
             menu.AddChild(mainMenu);
             menu.AddChild(reports);
-
             return menu;
-
         }
-
-
         public string GetHtml()
         {
             return Recursive(Root, 0);
         }
-
         public string Recursive(IEnumerable<Menu> menuList, int generation)
         {
             var str = new StringBuilder();
-          
             foreach (var menu in menuList)
             {
-
                 var childrens = new StringBuilder();
                 foreach (var item in menu.Children)
                 {
-                   
                     childrens.Append($@"
 	                <!--begin:Menu item-->
 					<div class=""menu-item"">
@@ -60,12 +46,8 @@ namespace System.Web.MVC
 						<!--end:Menu link-->
 					</div>
 					<!--end:Menu item-->            
-
-
                     ");
-                  
                 }
-
                 str.Append($@"
                     <!--begin:Menu item-->
 			<div data-kt-menu-trigger=""click"" class=""menu-item here show menu-accordion"">
@@ -78,29 +60,16 @@ namespace System.Web.MVC
 				<!--end:Menu link-->
 				<!--begin:Menu sub-->
 				<div class=""menu-sub menu-sub-accordion"">
-                    
 				{childrens}
-				
 				</div>
 				<!--end:Menu sub-->
 			</div>
 			<!--end:Menu item-->    
-
-                
                 ");
-
-
-
-
             }
-
-
             return str.ToString();
         }
-
-
     }
-
     public class Menu
     {
         public string Name { get; set; }
@@ -109,11 +78,9 @@ namespace System.Web.MVC
         public bool Blank { get; set; }
         public bool Visible { get; set; } = true;
         public List<Menu> Children { get; set; } = new List<Menu>();
-
         public Menu()
         {
         }
-
         public Menu(string Name, string Url = "#", string Icon = "", bool newBlank = false)
         {
             this.Name = Name;
@@ -121,7 +88,6 @@ namespace System.Web.MVC
             this.Icon = Icon;
             this.Blank = newBlank;
         }
-
         public void AddChild(Menu child)
         {
             Children.Add(child);
